1b7d08119315a3b02a3a5b79738cd0b9c1241a45
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-10 20:45:43 +0300

    chore: add "Card" interfaces

---
+++ src/interfaces/card.ts
@@ -0,0 +1,59 @@
+export namespace Card {
+  export interface IListItem {
+    Name: string
+    PromtCpin: string
+    Value1: string
+    Value2?: string
+    IsMasterPassMember: string
+    CardStatus: string
+    BankIca: string
+    LoyaltyCode: string
+    ProductName: string
+    UniqueId: string
+    EftCode: string
+  }
+  export interface IReqCardList {
+    token: string
+    msisdn: string
+    listType: string
+    sendSms: string
+  }
+  export interface IListRequest {
+    params: IReqCardList
+  }
+  export interface IReqCardRegister {
+    cardHolderName?: string | null
+    email?: string | null
+    lastName?: string | null
+    firstName?: string | null
+    homePostalCode?: string | null
+    homeCountryCode?: string | null
+    homeState?: string | null
+    homeCity?: string | null
+    homeAddress?: string | null
+    accountAliasName?: null
+    uiChannelType?: string
+    timeZone?: string
+    sendSms?: string
+    referenceNo?: string | null
+    mobileAccountConfig?: string
+    identityVerificationFlag?: string
+    mmrpConfig?: string
+    defaultAccount?: string
+    cpinFlag?: string
+    cardTypeFlag?: string
+    eActionType?: string
+    delinkReason?: string | null
+    clientIp?: string | null
+    actionType?: string
+    fp?: string | null
+    expiryDate: string
+    token: string
+    msisdn: string
+    cvc: string
+    rtaPan: string
+  }
+  export interface IRegisterRequest {
+    params: IReqCardRegister
+  }
+}
