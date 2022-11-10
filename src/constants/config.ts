e27800861901e4b7633cb7ae10fb7d461e47c139
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-10 20:13:22 +0300

    chore: add general config

---
+++ src/constants/config.ts
@@ -0,0 +1,12 @@
+import { IConfig } from "../interfaces/config";
+
+export const config: IConfig = {
+  serviceUrl: "",
+  clientId: "",
+  version: "36.4",
+  clientType: "1",
+  sendSmsLanguage: "tur",
+  referenceNo: "",
+  clientIp: "",
+  dateTime: new Date().toISOString(),
+};
---
+++ src/interfaces/config.ts
@@ -0,0 +1,10 @@
+export interface IConfig {
+  serviceUrl: string
+  clientId: string
+  sendSmsLanguage: string
+  referenceNo: string
+  clientIp: string
+  version: string
+  clientType: string
+  dateTime: string
+}
