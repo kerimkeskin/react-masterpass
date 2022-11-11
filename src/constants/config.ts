f16d3683a44d66ef1b267e4faa067bda825b67d9
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-11 16:44:27 +0300

    refactor: code format

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,12 +1,12 @@
-import { IConfig } from "../interfaces/config";
+import { IConfig } from '../interfaces/config'
 
 export const config: IConfig = {
-  serviceUrl: "",
-  clientId: "",
-  version: "36.4",
-  clientType: "1",
-  sendSmsLanguage: "tur",
-  referenceNo: "",
-  clientIp: "",
+  serviceUrl: '',
+  clientId: '',
+  version: '36.4',
+  clientType: '1',
+  sendSmsLanguage: 'tur',
+  referenceNo: '',
+  clientIp: '',
   dateTime: new Date().toISOString(),
-};
+}
