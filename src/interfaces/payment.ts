209544384bd6fbee6f980065c445c1e45aeb4faa
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-13 02:30:52 +0300

    chore: add "PurchaseAndRegister" method interfaces

--- src/interfaces/payment.ts
+++ src/interfaces/payment.ts
@@ -30,4 +30,38 @@ export namespace Payment {
     fp?: string | null
     referenceNo?: string
   }
+
+  export interface IPurchaseAndRegisterRequest {
+    params: IReqPurchaseAndRegister
+  }
+
+  export interface IReqPurchaseAndRegister {
+    token: string
+    msisdn: string
+    accountAliasName: string
+    rtaPan: string
+    expiryDate: string
+    cvc: string
+    amount: string
+    cardHolderName?: string
+    orderNo?: string
+    installmentCount?: number
+    rewardName?: string
+    rewardValue?: string
+    firstName?: string
+    lastName?: string
+    gender?: string
+    paymentType?: string
+    merchantId?: string
+    macroMerchantId?: string
+    actionType?: string
+    moneyCardInvoiceAmount?: string | null
+    moneyCardMigrosDiscountAmount?: string | null
+    moneyCardPaymentAmount?: string | null
+    moneyCardExtraDiscountAmount?: string | null
+    moneyCardProductBasedDiscountAmount?: string | null
+    sendSms?: string
+    fp?: string
+    referenceNo?: string
+  }
 }
