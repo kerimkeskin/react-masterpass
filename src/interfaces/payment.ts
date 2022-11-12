90646d45cd9cf787f6f70dabec0835231af439a0
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-12 22:32:53 +0300

    chore: add payment interfaces

---
+++ src/interfaces/payment.ts
@@ -0,0 +1,32 @@
+export namespace Payment {
+  export interface IPurchaseRequest {
+    params: IReqPurchase
+  }
+
+  export interface IReqPurchase {
+    orderNo: string
+    cvc: string
+    installmentCount: number
+    token: string
+    userId: string
+    msisdn: string
+    listAccountName: string
+    amount: string
+    aav?: string
+    sendSmsMerchant?: string
+    sendSms?: string
+    encCPin?: string
+    rewardValue?: string
+    rewardName?: string
+    paymentType?: string
+    macroMerchantId?: string
+    moneyCardInvoiceAmount?: string | null
+    moneyCardMigrosDiscountAmount?: string | null
+    moneyCardPaymentAmount?: string | null
+    moneyCardExtraDiscountAmount?: string | null
+    moneyCardProductBasedDiscountAmount?: string | null
+    password?: string
+    encPassword?: string
+    fp?: string | null
+  }
+}
